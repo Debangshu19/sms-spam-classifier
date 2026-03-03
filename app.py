1628aeac995e394671bee055cd592973a3ed98e9
Debangshu Dey <138014640+Debangshu19@users.noreply.github.com>
2026-03-03 17:01:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,28 @@
 import streamlit as st
 import pickle
 import string
-from nltk.corpus import stopwords
+import os
 import nltk
+from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 
+# ----- NLTK FIX FOR RENDER -----
+nltk_data_path = "/opt/render/nltk_data"
+os.makedirs(nltk_data_path, exist_ok=True)
+nltk.data.path.append(nltk_data_path)
+
+try:
+    nltk.data.find('tokenizers/punkt')
+except:
+    nltk.download('punkt', download_dir=nltk_data_path)
+
+try:
+    nltk.data.find('corpora/stopwords')
+except:
+    nltk.download('stopwords', download_dir=nltk_data_path)
+# --------------------------------
+
 ps = PorterStemmer()
-
-nltk.download('punkt')
-nltk.download('punkt_tab')
-
 
 def transform_text(text):
     text = text.lower()
